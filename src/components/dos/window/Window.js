495e0f7edd5b06d4efa9b0a20ea23668a74fc66f
Olfran Jiménez <olfran@gmail.com>
2018-08-21 23:18:34 -0400

    Updated some styles

--- src/components/dos/window/Window.js
+++ src/components/dos/window/Window.js
@@ -4,13 +4,9 @@ import './Window.css';
 export default function Window(props) {
   return (
     <div className="window" hidden={!props.visible}>
-      <div className="window-border1">
-        <div className="window-title">{props.title}</div>
-        <div className="window-border2">
-          <div className="window-wrap">
-            {props.children}
-          </div>
-        </div>
+      <div className="window-title">{props.title}</div>
+      <div className="window-wrap">
+        {props.children}
       </div>
     </div>
   );  
