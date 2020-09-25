b6b917b0535a7665818457c67d1f897dd25eb627
Olfran Jiménez <olfran@gmail.com>
2020-09-25 19:57:46 +0200

    Update clock componente.

--- src/components/dos/clock/Clock.js
+++ src/components/dos/clock/Clock.js
@@ -1,46 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
+function getTime() {
+  const date = new Date();
+  return {
+    hour: date.getHours(),
+    minutes: date.getMinutes(),
+    seconds: date.getSeconds(),
+    ampm: date.getHours() > 0 && date.getHours() < 13 ? 'AM' : 'PM'
+  };
+}
 
-export default class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    const date = new Date();
-    this.state = {
-      hour: date.getHours(),
-      minutes: date.getMinutes(),
-      seconds: date.getSeconds(),
-      ampm: date.getHours() > 0 && date.getHours() < 13 ? 'AM' : 'PM'
+function prefixZero(number) {
+  return number < 10 ? `0${number}` : number;
+}
+
+export default function Clock(props) {
+  const [time, setTime] = useState(getTime());
+
+  const updateTime = () => setTime(getTime());
+
+  useEffect(() => {
+    const intervalId = setInterval(updateTime, 1000); 
+    return function clearTimer() {
+      clearInterval(intervalId);
     };
-  }
+  });
 
-  updateTime = () => {
-    const date = new Date();
-    this.setState({
-      hour: date.getHours(),
-      minutes: date.getMinutes(),
-      seconds: date.getSeconds(),
-      ampm: date.getHours() >= 0 && date.getHours() < 13 ? 'AM' : 'PM'
-    });
-  }
-
-  prefixZero(number) {
-    return number < 10 ? `0${number}` : number;
-  }
-
-  componentWillMount() {
-    this.intervalId = setInterval(this.updateTime, 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.intervalId);
-  }
-
-  render() {
-    return (
-      <span>
-        {`${this.prefixZero(this.state.hour)}:${this.prefixZero(this.state.minutes)}:${this.prefixZero(this.state.seconds)} ${this.state.ampm}`}
-      </span>
-    );
-  }
-
+  return (
+    <span>
+      {`${prefixZero(time.hour)}:${prefixZero(time.minutes)}:${prefixZero(time.seconds)} ${time.ampm}`}
+    </span>
+  );
 }
